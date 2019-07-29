db846d95d3e119200708b15849f624054d1165b8
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-07-29 16:06:07 +0200

    started html and js files

---
+++ js/main.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const input = document.querySelector('.js-inputNumber');
+const button = document.querySelector('.js-buttonTry');
+const phrase = document.querySelector('.js-phraseSolution');
+let tries = document.querySelector('.js-triesNumber');
+
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+const myRandomNumber = getRandomNumber(100);
+console.log(`Mi número aleatorio es ${myRandomNumber}`);
+
+
+
+
+const isThisTheNumber = function () {
+    const number = input.value;
+    const validNumber = parseInt(number);
+    const numberTries = tries.HTML;
+    let validNumberTries = parseInt(numberTries);
+    if (validNumber === myRandomNumber) {
+        phrase.innerHTML = "¡HAS GANADO, CAMPEONA!";
+        let validNumberTries = `${validNumberTries + 1}`;
+    }
+    if (validNumber > myRandomNumber) {
+        phrase.innerHTML = "Demasiado alto"
+        let counter = tries + 1;
+    }
+    if (validNumber < myRandomNumber) {
+        phrase.innerHTML = "Demasiado bajo"
+        let counter = tries + 1;
+    }
+    if (validNumber < 0 || validNumber > 100) {
+        phrase.innerHTML = "Tiene que ser entre 0 y 100"
+        let counter = tries + 1;
+    }
+}
+
+
+button.addEventListener("click", isThisTheNumber)
+
